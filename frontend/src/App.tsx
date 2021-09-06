0cc351d5abe37474e674965d87e5437292a86f36
Ricardo Rodrigues <ricardo.rodriguesrds@gmail.com>
2021-09-06 16:00:47 -0300

    Static Charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
+import BarChart from 'components/BarChart';
 import DataTable from 'components/Datatable';
+import DonutChart from 'components/DonutChart';
 import Footer from 'components/Footer';
 import Navbar from 'components/NavBar';
 import React from 'react';
@@ -7,12 +9,25 @@ import React from 'react';
 function App() {
   return (
     <>
-      <Navbar/>
+      <Navbar />
       <div className="container">
-        <h1 className="text-primary">Olá Mundo !</h1>
-        <DataTable/>
+        <h1 className="text-primary px-3">Dashboard de Vendas</h1>
+        <div className="row px-3 py-1">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Taxa de Sucesso</h5>
+            <BarChart />
+          </div>
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Taxa de Sucesso</h5>
+            <DonutChart />
+          </div>
+        </div>
+        <div className="py-3">
+          <h2 className="text-primary">Todas Vendas</h2>
+        </div>
+        <DataTable />
       </div>
-      <Footer/>
+      <Footer />
     </>
   );
 }
