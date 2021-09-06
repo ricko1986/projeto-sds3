3f1b5a3690ab9273d334539110fbec6f4cd74b97
Ricardo Rodrigues <ricardo.rodriguesrds@gmail.com>
2021-09-06 14:34:32 -0300

    Bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <h1>Olá Mundo</h1>  
+      <h1 className="text-primary">Olá Mundo</h1>  
     </div>
   );
 }
